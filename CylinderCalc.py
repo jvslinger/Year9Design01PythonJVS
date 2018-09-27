730a7253323fbcf543aa477860507e5fdbd9cdb2
Jordan Van Slingerland <j.vanslingerland@Y22-Design-Jordan-Van-Slingerland.local>
2018-09-27 13:47:12 -0400

    GUI2

--- CylinderCalc.py
+++ CylinderCalc.py
@@ -1,4 +1,18 @@
 import tkinter as tk
+import math
+
+def submit():
+	print("submit pressed")
+	r = float(entr.get())
+	h = float(enth.get())
+
+	v = math.pi*r*r*h
+	v = round(v,3)
+
+	output.config(state="normal")
+	output.insert(tk.INSERT,v)
+	output.config(state="disabled")
+
 
 root = tk.Tk()
 root.title("Volume of a Cylinder")
@@ -15,7 +29,7 @@ labh.pack()
 enth = tk.Entry(root)
 enth.pack()
 
-btn = tk.Button(root, text = "Submit")
+btn = tk.Button(root, text = "Submit", command=submit)
 btn.pack()
 
 output = tk.Text(root, width=50, height=10, borderwidth=3, relief=tk.GROOVE)
