e98b415e5a80d61bed92ee1025fbf4eebda2b032
Jordan Van Slingerland <j.vanslingerland@Y22-Design-Jordan-Van-Slingerland.local>
2018-11-01 11:55:31 -0400

    DI Push 1

--- Contest Questions/CCC2018J1VJ.py
+++ Contest Questions/CCC2018J1VJ.py
@@ -1,1 +1,30 @@
 print("CCC2018J1VJ")
+
+a = input("Enter First Number now:  ")
+b = input("Enter Second Number now:  ")
+c = input("Enter Third Number now:  ")
+d = input("Enter Fourth Number now:  ")
+
+for i in range(1):
+
+	if a == 8:
+		print("Scammer")
+		break
+	if a == 9:
+		print("Scammer")
+		break
+	if b == c:
+		print("Scammer")
+		break
+	if c == b:
+		print("Scammer")
+		break
+	if d == 8:
+		print("Scammer")
+		break
+	if d == 9:
+		print("Scammer")
+		break
+	else:
+		print("Not a Scammer")
+
